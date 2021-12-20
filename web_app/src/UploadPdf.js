493b91eeffdb8b1bd10a83668e71638df3a93a9c
Ferhat Açıkalın <ferhatackln@gmail.com>
2021-12-20 03:34:33 +0300

    front end dosya yükleme

--- web_app/src/UploadPdf.js
+++ web_app/src/UploadPdf.js
@@ -1,37 +1,75 @@
+import axios from "axios";
+import React from "react";
+
 export default function UploadPdf() {
+  const [selectedFile, setSelectedFile] = React.useState(null);
+  const [msg, setMsg] = React.useState('');
+
+  const handleClick = async (event) => {
+    setMsg("Proje Yükleniyor")
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("pdf", selectedFile);
+    try {
+      const response = await axios({
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "Authorization": "Bearer " + localStorage.getItem("access_token"),
+        },
+        method: "post",
+        url: "http://localhost:8080/api/v1/upload_project",
+        data: formData,
+      });
+      if(response.status===200){
+          setMsg("Proje Yüklendi");
+      }
+      else{
+        setMsg("Proje yüklenirken hata oluştu");
+      }
+    } catch (error) {
+      setMsg("Proje yüklenirken hata oluştu");
+      console.log(error);
+    }
+  };
+
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
   return (
-    <div class="bg-white rounded-lg shadow sm:max-w-md sm:w-full sm:mx-auto sm:overflow-hidden">
-      <div class="px-4 py-8 sm:px-10">
-        <div class="relative mt-6">
-          <div class="absolute inset-0 flex items-center">
-            <div class="w-full border-t border-gray-300"></div>
+    <div className="bg-white rounded-lg shadow sm:max-w-md sm:w-full sm:mx-auto sm:overflow-hidden">
+      <div className="px-4 py-8 sm:px-10">
+        <div className="relative mt-6">
+          <div className="absolute inset-0 flex items-center">
+            <div className="w-full border-t border-gray-300"></div>
           </div>
-          <div class="relative flex justify-center text-sm leading-5">
-            <span class="px-2 text-gray-500 bg-white">Pdf Yükle </span>
+          <div className="relative flex justify-center text-sm leading-5">
+            <span className="px-2 text-gray-500 bg-white">Pdf Yükle </span>
           </div>
         </div>
-        <div class="mt-6">
-          <div class="w-full space-y-6">
-
-            <div class="w-full">
-              <div class=" relative ">
+        <div className="mt-6">
+          <div className="w-full space-y-6">
+            <div className="w-full">
+              <div className=" relative ">
                 <input
                   type="file"
                   id="file"
-                  
-                  class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                  onChange={handleFileSelect}
+                  className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                   placeholder="Dosya"
                 />
               </div>
             </div>
             <div>
-              <span class="block w-full rounded-md shadow-sm">
+              <span className="block w-full rounded-md shadow-sm">
                 <button
+                onClick={handleClick}
                   type="button"
-                  class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                  className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
                 >
                   Yükle
                 </button>
+                {msg}
+              
               </span>
             </div>
           </div>
